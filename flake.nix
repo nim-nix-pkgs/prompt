ede8108275404d58688257aad19b6e54522dbffb
hugosenari <hugosenari@gmail.com>
2022-03-12 04:06:15 -0300

    chore: re index prompt

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Feature-rich readline replacement'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."prompt-master".url = "path:./master";
+  inputs."prompt-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."prompt-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
